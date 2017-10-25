0e75971f570dddacbfea0f395ddf3edebc6d5265
Rahul Sharma <er.csrahul@gmail.com>
2017-10-25 17:43:32 +0530

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Namastey Duniya\n');
+  res.send('Hello Sourcefuse\n');
 });
 
 app.listen(PORT, HOST);
